bae903a74f76cd02fa50088286fc387899735f50
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-02-23 00:02:10 +0600

    Implement NavBar component with navigation links, add MongoDB dependency, and initialize database connection

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,7 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function NavBar() {
+  const navMenu = () => {
+    return (
+      <>
+        {" "}
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/services"}>Services</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/contact"}>Contact</Link>
+        </li>
+      </>
+    );
+  };
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -26,23 +49,7 @@ export default function NavBar() {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navMenu()}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">
@@ -55,30 +62,10 @@ export default function NavBar() {
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navMenu()}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Appointment</a>
       </div>
     </div>
   );
