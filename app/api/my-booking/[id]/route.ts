4111c9f126f44b5c45239ef8bf654e3dbeb81a30
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-16 23:39:39 +0600

    Implement owner validation for booking retrieval; return 403 status for unauthorized access

--- app/api/my-booking/[id]/route.ts
+++ app/api/my-booking/[id]/route.ts
@@ -10,8 +10,17 @@ import { authOptions } from "../../auth/[...nextauth]/route";
 export const GET = async (req: NextResponse, { params }: { params: { id: string } }) => {
     const { id } = await params
     const bookingCollection = dbConnect(collectionNameObj.bookingCollection)
-    const singleBooking = await bookingCollection.findOne({ _id: new ObjectId(id) })
-    return NextResponse.json(singleBooking)
+    const session = await getServerSession(authOptions)
+    const email = session?.user?.email
+    const currentBooking = await bookingCollection.findOne({ _id: new ObjectId(id) })
+    const isOwnerOk = email === currentBooking?.email
+    if (isOwnerOk) {
+        const singleBooking = await bookingCollection.findOne({ _id: new ObjectId(id) })
+        return NextResponse.json(singleBooking)
+    } else {
+        return NextResponse.json({ message: 'Forbidden update action' }, { status: 403 })
+    }
+
 }
 export const PATCH = async (req: NextResponse, { params }: { params: Params }) => {
     const { id } = await params
