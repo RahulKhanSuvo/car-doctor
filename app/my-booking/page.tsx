d58d511aefdb0920bac7c4d3fdbc5ef6101c310c
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-16 19:15:52 +0600

    Fetch and display user booking data on My Booking page; implement data fetching and rendering logic

--- app/my-booking/page.tsx
+++ app/my-booking/page.tsx
@@ -1,6 +1,36 @@
 "use client";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 export default function MyBooking() {
-  return <div>MyBooking</div>;
+  interface Booking {
+    _id: string;
+    name: string;
+    img: string;
+  }
+
+  const [data, setData] = useState<Booking[]>([]);
+  useEffect(() => {
+    const myBookingData = async () => {
+      const res = await fetch("http://localhost:3000/api/service");
+      const d = await res.json();
+      setData(d);
+    };
+    myBookingData();
+  }, []);
+  console.log(data);
+  return (
+    <div className="container mx-auto">
+      <h1>Cart </h1>
+      <div>
+        {data.map((item) => {
+          return (
+            <div key={item._id}>
+              <Image alt="naf" width={100} height={100} src={item.img} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
